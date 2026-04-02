983e42f278d8438c1d1534442dffe590ea3b0e24
David Fooks <david.fooks@larian.com>
2026-04-02 09:45:03 +0800

    Update GridVertex attribute name to match renderer

--- data/shaders/GridVertex.glsl
+++ data/shaders/GridVertex.glsl
@@ -1,7 +1,7 @@
 #version 300 es
 precision mediump float;
 
-layout (location = 0) in vec2 position;
+layout (location = 0) in vec2 uv;
 out vec2 worldPos;
 
 uniform mat4 viewProjection;
@@ -9,7 +9,7 @@ uniform vec4 box;
 
 void main()
 {
-    vec2 p = mix(box.xy, box.zw, position);
+    vec2 p = mix(box.xy, box.zw, uv);
     worldPos = p;
     gl_Position = viewProjection * vec4(p, 0.0, 1.0);
 }
